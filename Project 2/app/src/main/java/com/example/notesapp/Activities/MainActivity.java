6e8440d013fd1ced60d3e9fc52525e9fb7b246ec
Jack Sebahar <jacksebahar@gmail.com>
2023-11-15 16:03:40 -0600

    home page firebase stuff

--- Project 2/app/src/main/java/com/example/notesapp/Activities/MainActivity.java
+++ Project 2/app/src/main/java/com/example/notesapp/Activities/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.notesapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.example.notesapp.R;
 
-import com.firebase.ui.auth.AuthUI;
+import com.example.notesapp.adapter.NotesAdapter;
+import com.example.notesapp.util.FirebaseUtil;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -14,14 +22,62 @@ import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
 
 
-
 public class MainActivity extends AppCompatActivity {
 
+    private static final int LIMIT = 50;
+    private static final String TAG = "MainActivity";
+    private ViewGroup mEmptyView;
+
+
+    private RecyclerView NotebooksRecycler;
+
+    private FirebaseFirestore mFirestore;
+    private NotesAdapter mAdapter;
+    private Query mQuery;
+    private NotesAdapter.OnNoteSelectedListener listener;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        NotebooksRecycler = findViewById(R.id.recycler_notebooks);
+
+        FirebaseFirestore.setLoggingEnabled(true);
+        mFirestore = FirebaseUtil.getFirestore();
+
+        mQuery = mFirestore.collection("users");
+        initRecyclerView();
+
+    }
 
 
+    private void initRecyclerView() {
+        if (mQuery == null) {
+            Log.w(TAG, "No query, not initializing RecyclerView");
+        }
+
+        mAdapter = new NotesAdapter(mQuery, listener) {
+
+            @Override
+            protected void onDataChanged() {
+                // Show/hide content if the query returns empty.
+                if (getItemCount() == 0) {
+                    NotebooksRecycler.setVisibility(View.GONE);
+                } else {
+                    NotebooksRecycler.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            protected void onError(FirebaseFirestoreException e) {
+                // Show a snackbar on errors
+                Snackbar.make(findViewById(android.R.id.content),
+                        "Error: check logs for info.", Snackbar.LENGTH_LONG).show();
+            }
+        };
+
+        NotebooksRecycler.setLayoutManager(new LinearLayoutManager(this));
+        NotebooksRecycler.setAdapter(mAdapter);
     }
 }
