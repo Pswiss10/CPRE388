ec55218e0d3590aac2ce12b1b891e62564a7f803
crbaes <crbaes@iastate.edu>
2023-11-14 22:09:48 -0600

    this took SO much longer than i expected

--- Project 2/app/src/main/java/com/example/notesapp/Firebase/Login.java
+++ Project 2/app/src/main/java/com/example/notesapp/Firebase/Login.java
@@ -1,15 +1,40 @@
 package com.example.notesapp.Firebase;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.notesapp.R;
 
 public class Login extends AppCompatActivity {
+    Button createAccountButton;
+    Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        createAccountButton = findViewById(R.id.createButton);
+        loginButton = findViewById(R.id.loginButton);
+
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createAccountButton.setText("WHY");
+                Intent intent = new Intent(Login.this, CreateAccount.class);
+                startActivity(intent);
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
     }
 }
