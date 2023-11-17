380335f0bf0a639d149c9fd8bfa7188f8d7c65e3
Jack Sebahar <jacksebahar@gmail.com>
2023-11-17 12:18:17 -0600

    merging branch stuff

---
+++ Project 2/app/src/main/java/com/example/notesapp/adapter/NotesAdapter.java
@@ -0,0 +1,99 @@
+/**
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.notesapp.adapter;
+
+import android.content.res.Resources;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import com.bumptech.glide.Glide;
+import com.example.notesapp.Models.Notes;
+import com.example.notesapp.R;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.Query;
+
+public class NotesAdapter extends FirestoreAdapter<NotesAdapter.ViewHolder> {
+
+    public interface OnNoteSelectedListener {
+
+        void onNoteSelected(DocumentSnapshot note);
+
+    }
+
+    private OnNoteSelectedListener mListener;
+
+    public NotesAdapter(Query query, OnNoteSelectedListener listener) {
+        super(query);
+        mListener = listener;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        return new ViewHolder(inflater.inflate(R.layout.item_notebook, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.bind(getSnapshot(position), mListener);
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageView;
+        TextView nameView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.notebook_item_image);
+            nameView = itemView.findViewById(R.id.notebook_item_name);
+        }
+
+        public void bind(final DocumentSnapshot snapshot,
+                         final OnNoteSelectedListener listener) {
+
+            Notes notes = snapshot.toObject(Notes.class);
+            Resources resources = itemView.getResources();
+
+            // Load image
+            Glide.with(imageView.getContext())
+                    .load(notes.getPhoto())
+                    .into(imageView);
+
+            nameView.setText(notes.getName());
+
+
+            // Click listener
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (listener != null) {
+                        listener.onNoteSelected(snapshot);
+                    }
+                }
+            });
+        }
+
+    }
+}
